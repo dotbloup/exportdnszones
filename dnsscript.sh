3b87e7744332971d5ca26bc23508e768a9a780e8
dotbloup <nguerinet@gmail.com>
2018-07-02 11:25:35 +0200

    Update dnsscript.sh

--- dnsscript.sh
+++ dnsscript.sh
@@ -5,6 +5,6 @@ rm export.list
 fi
 for domain in `cat domains.list`; do
 domain1="${domain::-3}"
-echo "zone \"$domain1\" { type slave; file \"slaves/$domain\"; masters { 85.17.26.201; }; };" >> export.list
+echo "zone \"$domain1\" { type slave; file \"slaves/$domain\"; masters { XX.XX.26.201; }; };" >> export.list
 done
 
